15a80709b49cc1d5083db3e0ffdc9cdec1c2bea6
Julius Franke <julius.franke@gmx.net>
2024-12-02 17:56:52 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+# setup(name='diffmp',
+#       version='0.1',
+#       description='Generate Motion Primitives with Diffusion',
+#       # url='http://github.com/storborg/funniest',
+#       author='Julius Franke',
+#       author_email='julius.franke@gmx.net',
+#       # license='MIT',
+#       packages=['diffmp'],
+#       zip_safe=False)
+
+if __name__ == '__main__':
+    setuptools.setup()
