8aad5a8e6f978e026096eed23d4e82c9bccfe90d
Julius Franke <julius.franke@gmx.net>
2024-12-18 23:34:18 +0100

    simple test

--- tests/test_dynamics.py
+++ tests/test_dynamics.py
@@ -1,24 +1,27 @@
-from diffmp.dynamics import get_dynamics
-from icecream import ic
+# from diffmp.dynamics import get_dynamics
+import diffmp
+import pytest
 import numpy as np
 import yaml
 
 
-def test():
-    with open("unicycle1_v0.yaml", "r") as file:
-        data = yaml.safe_load(file)
+@pytest.mark.parametrize("dynamics", [("unicycle1_v0"), ("unicycle1_v1")])
+def test_implementations(dynamics: str):
+    assert isinstance(diffmp.dynamics.get_dynamics(dynamics), diffmp.dynamics.DynamicsBase)
 
-    with open("unicycle2_v0.yaml", "r") as file:
-        data_2 = yaml.safe_load(file)
+    # with open("unicycle1_v0.yaml", "r") as file:
+    #     data = yaml.safe_load(file)
 
-    a = get_dynamics(data)
-    b = get_dynamics(data_2)
-    u= np.array([0.3,0.5])
-    q = np.array([0,0,0])
-    q2 = np.array([0,0,0,0,0])
-    u2 = np.array([0.1,0.1])
-    for i in range(55):
-        # q = a.step(q=q, u=u)
-        q2 = b.step(q=q2, u=u2)
-        print(q2)
+    # with open("unicycle2_v0.yaml", "r") as file:
+    #     data_2 = yaml.safe_load(file)
 
+    # a = get_dynamics(data)
+    # b = get_dynamics(data_2)
+    # u= np.array([0.3,0.5])
+    # q = np.array([0,0,0])
+    # q2 = np.array([0,0,0,0,0])
+    # u2 = np.array([0.1,0.1])
+    # for i in range(55):
+    #     # q = a.step(q=q, u=u)
+    #     q2 = b.step(q=q2, u=u2)
+    #     print(q2)
