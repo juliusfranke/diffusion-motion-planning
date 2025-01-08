50f4f47a1db6c4f2f6822c93d43a6a4ff04ed640
Julius Franke <julius.franke@gmx.net>
2025-01-08 23:31:24 +0100

    load config from yaml

--- scripts/script_train.py
+++ scripts/script_train.py
@@ -4,31 +4,14 @@ import diffmp
 
 
 def train():
-    dynamics = diffmp.dynamics.get_dynamics("unicycle1_v0")
     instance = diffmp.problems.Instance.from_dict(
         diffmp.utils.load_yaml(Path("data/instances/alcove_unicycle_single.yaml"))
     )
-    config = diffmp.torch.Config(
-        dynamics=dynamics,
-        timesteps=5,
-        problem="bugtrap",
-        n_hidden=2,
-        s_hidden=128,
-        regular=[diffmp.utils.ParameterRegular.actions],
-        conditioning=[],
-        loss_fn=diffmp.torch.Loss.mse,
-        dataset=Path("data/training_datasets/alcove.parquet"),
-        denoising_steps=30,
-        batch_size=100,
-        lr=1e-3,
-        dataset_size=1000,
-        reporters=[diffmp.utils.TQDMReporter()],
-        noise_schedule=diffmp.torch.NoiseSchedule.linear,
-    )
+    config = diffmp.torch.Config.from_yaml(Path("scripts/conf.yaml"))
     model = diffmp.torch.Model(config)
     model.path = Path("data/models/test.pt")
     diffmp.torch.train(model, 100)
-    # model.save()
+    model.save()
     a = diffmp.torch.sample(model, 100, instance)
     print(a)
 
